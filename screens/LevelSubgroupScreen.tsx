c4e22639b7c2961be10b60c67e90e4c90145a670
Chitour1 <chitour1@gmail.com>
2025-08-04 10:43:17 +0300

    Update LevelSubgroupScreen.tsx

--- screens/LevelSubgroupScreen.tsx
+++ screens/LevelSubgroupScreen.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { useApp } from '../contexts/AppContext';
+import { useAppContext } from '../contexts/AppContext';
 import { Screen, LibraryGroup, LessonLibrary } from '../types';
 import ScreenContainer from '../components/common/ScreenContainer';
 import BackButton from '../components/common/BackButton';
