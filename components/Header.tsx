b2e8baac899088a905316a145c95809c423ae009
Chitour1 <chitour1@gmail.com>
2025-08-04 10:48:56 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useApp } from '../contexts/AppContext';
+import { useAppContext } from '../contexts/AppContext';
 
 const LogoIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="stroke-accent w-7 h-7">
